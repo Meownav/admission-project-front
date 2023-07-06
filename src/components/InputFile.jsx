17485ca28d6fc0152458e7abe9868e1aa0b7bd55
Meownav <manavchauhan124@gmail.com>
2023-07-06 16:36:28 +0530

    Attempt to connect to backend for datesheet.(Fail)

--- src/components/InputFile.jsx
+++ src/components/InputFile.jsx
@@ -3,95 +3,120 @@ import axios from "axios";
 import "./InputFile.css";
 
 const InputFile = () => {
-	const [file, setFile] = useState(null);
-	const [message, setMessage] = useState("");
+  const [file, setFile] = useState(null);
+  const [message, setMessage] = useState("");
+  const [datesheet, setDatesheet] = useState([]);
 
-	const handleFileChange = (event) => {
-		setFile(event.target.files[0]);
-	};
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		if (!file) {
-			setMessage("Please select a file.");
-			return;
-		}
+  const handleDatesheetChange = (event) => {
+    console.log(event.target.files);
+    for (let i = 0; i < event.target.files.length; i++) {
+      setDatesheet((prevDatesheet) => [
+        ...prevDatesheet,
+        event.target.files[i],
+      ]);
+    }
+  };
 
-		const formData = new FormData();
-		formData.append("file", file);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!file && !datesheet) {
+      setMessage("Please select a file.");
+      return;
+    }
 
-		axios
-			.post("http://localhost:5000/process-file", formData)
-			.then((response) => {
-				console.log(response.data);
-				setMessage(response.data.message);
-				downloadResult(response.data.result_path); // Pass the result path for downloading
-			})
-			.catch((error) => {
-				setMessage("An error occurred during file processing.");
-				console.log(error);
-			});
-	};
+    const formData = new FormData();
+    // if students file is provided and datesheet is not provided
+    if (file && !datesheet) {
+      formData.append("file", file);
+      axios
+        .post("http://localhost:5000/process-file", formData)
+        .then((response) => {
+          setMessage(response.data.message);
+          downloadResult(response.data.result_path); // Pass the result path for downloading
+        })
+        .catch((error) => {
+          setMessage(
+            "An error occurred during file processing. Check console."
+          );
+          console.log(error);
+        });
+    }
+    // if students file is not provided and datesheet is provided
+    if (datesheet && !file) {
+      formData.append("datesheet", datesheet);
+      axios
+        .post("http://localhost:5000/process-datesheet", formData)
+        .then((response) => {
+          console.log("Form data was : ", formData);
+        })
+        .catch((err) => {
+          setMessage(err.message);
+        });
+    }
+  };
 
-	const downloadResult = (resultPath) => {
-		console.log("In DLres, result path is ", resultPath);
-		axios
-			.get("http://localhost:5000/download-result", {
-				params: {
-					resultPath: resultPath,
-				},
-				responseType: "blob", // Set the response type to 'blob'
-			})
-			.then((response) => {
-				const url = window.URL.createObjectURL(new Blob([response.data]));
-				const link = document.createElement("a");
-				link.href = url;
-				link.setAttribute("download", resultPath.split("/").pop());
-				document.body.appendChild(link);
-				link.click();
-				link.remove();
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+  const downloadResult = (resultPath) => {
+    axios
+      .get("http://localhost:5000/download-result", {
+        params: {
+          resultPath: resultPath,
+        },
+        responseType: "blob", // Set the response type to 'blob'
+      })
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", resultPath.split("/").pop());
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-	return (
-		<div className="container">
-			<div className="section">
-				<h2 className="section-heading">Enter File</h2>
-				<hr></hr>
-				<form className="form" onSubmit={handleSubmit}>
-					<input
-						className="form-input"
-						type="file"
-						accept=".xls,.xlsx"
-						onChange={handleFileChange}
-					/>
-					{/* <hr></hr> */}
-					<button className="form-button" type="submit">
-						Process File
-					</button>
-					<input
-						type="file"
-						className="form-input2"
-						id=""
-						accept="application/pdf"
-					/>
-					<button className="form-button3" type="submit">
-						Process Datesheet
-					</button>
-				</form>
-				<p className="message">{message}</p>
-			</div>
-			{/* <hr></hr> */}
-			<div className="outer">
-				<button className="form-button2" type="submit">
-					Assign Teacher Duties
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="section">
+        <h2 className="section-heading">Enter File</h2>
+        <hr></hr>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            className="form-input"
+            type="file"
+            accept=".xls,.xlsx"
+            onChange={handleFileChange}
+          />
+          <button className="form-button" type="submit">
+            Process File
+          </button>
+          <input
+            type="file"
+            className="form-input2"
+            id=""
+            accept="application/pdf"
+            multiple
+            onChange={handleDatesheetChange}
+          />
+          <button className="form-button3" type="submit">
+            Process Datesheet
+          </button>
+        </form>
+        <p className="message">{message}</p>
+      </div>
+      <div className="outer">
+        <button className="form-button2" type="submit">
+          Assign Teacher Duties
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default InputFile;
--- src/components/teacherDuty.jsx
+++ src/components/teacherDuty.jsx
@@ -3,38 +3,25 @@ import React, { useState } from "react";
 import "./teacherDuty.css";
 
 const TeacherDuty = () => {
-	return (
-		<div className="container">
-			<div className="section">
-				<h2 className="section-heading">
-					Teacher Duties<hr></hr>
-				</h2>
-				{/* <form className="form"> onSubmit={handleSubmit}> */}
-				<form className="form">
-					Teacher Details
-					<input
-						className="form-input"
-						type="file"
-						// onChange={handleFileChange}
-					/>
-					Exam Details
-					<input
-						className="form-input"
-						type="file"
-						// onChange={handleFileChange}
-					/>
-					{/* <button className="form-button" type="submit">
-            Assign Duties
-          </button> */}
-				</form>
-				<hr></hr>
-				<button className="form-button" type="submit">
-					Assign Duties
-				</button>
-				{/* <p className="message">{message}</p> */}
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+      <div className="section">
+        <h2 className="section-heading">
+          Teacher Duties<hr></hr>
+        </h2>
+        <form className="form">
+          Teacher Details
+          <input className="form-input" type="file" />
+          Exam Details
+          <input className="form-input" type="file" />
+        </form>
+        <hr></hr>
+        <button className="form-button" type="submit">
+          Assign Duties
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TeacherDuty;
