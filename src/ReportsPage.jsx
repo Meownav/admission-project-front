f4dc8f03ea6ce9b71ed5ceb0b30ac9bbe7ff3ba8
manpreetnub23 <singhmanpreet0107@gmail.com>
2023-09-25 21:20:17 +0530

    report 1 table formatted

--- src/ReportsPage.jsx
+++ src/ReportsPage.jsx
@@ -3,12 +3,12 @@ import axios from "axios";
 
 const ReportsPage = () => {
   const [data, setData] = useState([]);
-
   const fetchData = async () => {
     try {
       const response = await axios.get("http://localhost:5000/get_report");
       if (response.data.message === "Success") {
         const jsonData = JSON.parse(response.data.data);
+        console.log(jsonData);
         setData(jsonData);
       }
     } catch (error) {
@@ -28,7 +28,7 @@ const ReportsPage = () => {
           <div className="button">
             <button onClick={fetchData}>Create Report 1</button>
           </div>
-          <div className="button">
+          {/* <div className="button">
             <button>button 2</button>
           </div>
           <div className="button">
@@ -51,29 +51,49 @@ const ReportsPage = () => {
           </div>
           <div className="button">
             <button>button 9</button>
-          </div>
+          </div> */}
         </div>
       </div>
 
-      <h1>Report</h1>
-      <table>
-        <thead>
-          <tr >
-            <th>Programme Name</th>
-            <th>Paper Type</th>
-            <th>Count</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((value, index) => (
+      <center>
+        <h1 id="report-heading">Report</h1>
+      </center>
+      <center>
+        <table id="tbl">
+          <thead>
+            <tr>
+              <th>Programme Name</th>
+              <th>AEC</th>
+              <th>Core</th>
+              <th>DSC</th>
+              <th>GE</th>
+              <th>SEC</th>
+              <th>VAC</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((val, idx) => (
+              <tr key={idx} >
+                <td key={"index" + idx}>{val["index"]}</td>
+                <td key={"aec" + idx}>{val["AEC"]}</td>
+                <td key={"core" + idx}>{val["CORE"]}</td>
+                <td key={"dsc" + idx}>{val["DSC"]}</td>
+                <td key={"ge" + idx}>{val["GE"]}</td>
+                <td key={"sec" + idx}>{val["SEC"]}</td>
+                <td key={"vac" + idx}>{val["VAC"]}</td>
+              </tr>
+            ))}
+
+            {/* {data.map((value, index) => (
             <tr key={index}>
               <td>{value["Programme Name"]}</td>
               <td>{value["Paper Type"]}</td>
               <td>{value["count"]}</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          ))} */}
+          </tbody>
+        </table>
+      </center>
     </div>
   );
 };
